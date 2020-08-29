6a14045170fabc74bc8cce6693e8a6146e14012b
Sergio <sergio.gundin.rodriguez@gmail.com>
2020-08-29 21:18:57 +0200

    added docs

---
+++ docs/precache-manifest.7f888bb4ba4ff61bc51a1901338fea8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea684a67f730e3c045e87d597ceadd5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bfe80af4e810ac0ca83",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "81d6c01386ea4dcb230e",
+    "url": "/static/css/main.12eefa88.chunk.css"
+  },
+  {
+    "revision": "0bfe80af4e810ac0ca83",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81d6c01386ea4dcb230e",
+    "url": "/static/js/main.524d8b69.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
