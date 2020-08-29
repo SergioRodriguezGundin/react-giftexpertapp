be724aab3a898863ae9ba53cbb846c673ef63ed8
Sergio <sergio.gundin.rodriguez@gmail.com>
2020-08-29 21:17:50 +0200

    first commint

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GiftGrid } from "./components/GiftGrid";
+
+const GiftExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  return (
+    <>
+      <h2>GiftExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category) => (
+          <GiftGrid 
+            category={category}
+            key={category}>
+          </GiftGrid>
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default GiftExpertApp;
